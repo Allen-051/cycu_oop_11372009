ddf4ef5f54e8a667d19ad588dc316b04fd8bfac4
邱昱倫11372009 <0501yulunallen@gmail.com>
2025-02-25 20:33:12 +0800

    Create Exercise2.11.3.py

---
+++ 20250225/Exercise2.11.3.py
@@ -0,0 +1,24 @@
+# PART 1
+import math
+r = 5
+r3 = math.pow(5,3)
+sphere_volume = 4/3*(math.pi*r3)
+print(f'The result of sphere volume is {sphere_volume}')
+
+# PART 2
+x = 100
+COS_2 = math.cos(x)**2
+SIN_2 = math.sin(x)**2
+result = COS_2 + SIN_2
+print(f'The result is {result}.')
+
+# PART 3
+e_2_1= math.e ** 2
+print(e_2_1)
+
+e_2_2= math.pow(math.e, 2)
+print(e_2_2)
+
+x = 2
+e_2_3 = math.exp(x)
+print(e_2_3)
